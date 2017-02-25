1e901083cce0d1c67d1ae703a65bb7881e3d91ff
Caroline Haigh <ch26g13@soton.ac.uk>
2017-02-25 19:19:11 +0000

    Added chord progression

--- play.js
+++ play.js
@@ -15,10 +15,24 @@ var pianoSettings = {
 				"release": 1,
 			},
 			"portamento": 0.01,
-			"volume": 0
+			"volume": -10
 		};
 
 var piano = new Tone.Synth(pianoSettings).toMaster();	
+
+//a polysynth composed of 6 Voices of Synth
+var synth = new Tone.PolySynth(4, Tone.Synth).toMaster();
+//set the attributes using the set interface
+synth.set(pianoSettings);
+//play a chord
+//synth.triggerAttackRelease(["D4", "Gb4", "A5", "D5"], "4n");
+//synth.triggerAttackRelease(["A4", "Db4", "E4", "A5"], "4n");
+
+var cprog = new Tone.Pattern(function(time, note){
+    synth.triggerAttackRelease(note, '2n');
+}, [["D4", "Gb4", "A5", "D5"], ["A4", "Db4", "E4", "A5"], ["B4", "D4", "Gb4", "B5"], ["G4", "B4", "D4", "G5"] ], 'up');
+		
+cprog.start(0);		
 		
 var psynth = new Tone.PluckSynth().toMaster();
 
@@ -26,15 +40,15 @@ var ppattern = new Tone.Pattern(function(time, note){
     psynth.triggerAttackRelease(note, '4n');
 }, ["C3", "E4", "G5", "A4"], 'randomWalk');
  
-ppattern.start(0);
+ppattern.start(2.25);
 
 
 var dsynth = new Tone.MembraneSynth().toMaster();
 
 var dpattern = new Tone.Pattern(function(time, note){
     dsynth.triggerAttackRelease(note, '4n');
-}, ["C3", "E3", "G3", "A3"]);
+}, ["D3", "A3", "B3", "G3"]);
  
-dpattern.start(0.25);
+dpattern.start(2.0);
 
 Tone.Transport.start();
