aab076323e8f5507e6cb93683c347c217600c469
Caroline Haigh <ch26g13@soton.ac.uk>
2017-02-25 14:39:46 +0000

    Bleep bloop bloop bleep

--- play.js
+++ play.js
@@ -1,12 +1,20 @@
-var vib = new Tone.Vibrato()
-
 //create a synth and connect it to the master output (your speakers)
-var synth = new Tone.PluckSynth().connect(vib).toMaster();
 
-var pattern = new Tone.Pattern(function(time, note){
-    synth.triggerAttackRelease(note, 8n);
-}, ["C3", "E4", "G5", "A4"]);
+var psynth = new Tone.PluckSynth().toMaster();
 
-pattern.start(0);
+var ppattern = new Tone.Pattern(function(time, note){
+    psynth.triggerAttackRelease(note, '4n');
+}, ["C3", "E4", "G5", "A4"], 'randomWalk');
+ 
+ppattern.start(0);
+
+
+var dsynth = new Tone.MembraneSynth().toMaster();
+
+var dpattern = new Tone.Pattern(function(time, note){
+    dsynth.triggerAttackRelease(note, '4n');
+}, ["C3", "E3", "G3", "A3"]);
+ 
+dpattern.start(0.25);
 
 Tone.Transport.start();
