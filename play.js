e71f92c8c1d6afc989e467c58005d97e8e23f433
Caroline Haigh <ch26g13@soton.ac.uk>
2017-02-25 17:14:51 +0000

    Probably did stuff

--- play.js
+++ play.js
@@ -1,5 +1,25 @@
 //create a synth and connect it to the master output (your speakers)
 
+var pianoSettings = {
+			"oscillator": {
+				"detune": 0,
+				"type": "custom",
+				"partials" : [2, 1, 2, 2],
+				"phase": 0,
+				"volume": 0
+			},
+			"envelope": {
+				"attack": 0.005,
+				"decay": 0.3,
+				"sustain": 0.2,
+				"release": 1,
+			},
+			"portamento": 0.01,
+			"volume": 0
+		};
+
+var piano = new Tone.Synth(pianoSettings).toMaster();	
+		
 var psynth = new Tone.PluckSynth().toMaster();
 
 var ppattern = new Tone.Pattern(function(time, note){
